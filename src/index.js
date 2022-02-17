c8df5909c5173df75bcd889cc13c7f764206cc6a
rranjan6 <rajeevbankey@gmail.com>
2022-02-17 11:14:46 +0530

    using img ifter importing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
-//import logo from './image/logo192.png'
+import logo from './image/logo192.png'
 
 function Footer(){
     return(
@@ -13,7 +13,7 @@ function Page(){
    return(
    <div>
         <Header/>
-        <img src="../pic/logo193.png" width="50px"/>
+        <img src={logo} width="50px"/>
         <h2>This is h2 tag</h2>
         <p>This is a paragraph</p>
         <Footer/>
