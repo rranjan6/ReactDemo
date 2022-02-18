e1bd45f5b262365687d65f929ff3f3d3dfe31561
rranjan6 <rajeevbankey@gmail.com>
2022-02-19 00:17:38 +0530

    Add logo in Navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,9 +3,7 @@ import React from "react";
 export default function Navbar() {
     return(
         <nav>
-            <img src="logo193.png" height="80px"/>
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src="images/airbnb-logo.png"/>
         </nav>
     )
 }
