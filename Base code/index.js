58dac95f184c56762191952710bd3f44ecdcd725
rranjan6 <rajeevbankey@gmail.com>
2022-02-20 08:51:10 +0530

    JS inside JSX

---
+++ Base code/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+ReactDOM.render(
+  <h1>React Basics</h1>,
+  document.getElementById('root')
+);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function App() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+
+  if(hours<12)timeOfDay="Good Morning"
+  else if(hours>=12 && hours<=17) {
+    timeOfDay="Good Afternoon"
+  }
+  else{
+    timeOfDay="Good Night"
+  }
+
+  return (
+    <h1>{timeOfDay}</h1>
+  )
+}
+
 ReactDOM.render(
-  <h1>React Basics</h1>,
+  <h1><App/></h1>,
   document.getElementById('root')
 );
