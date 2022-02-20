acf7f7446b9f01bf0c801ea4844dae365ca9399b
rranjan6 <rajeevbankey@gmail.com>
2022-02-20 08:18:47 +0530

    React Basecode

---
+++ cardcomponent/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './image/logo192.png'
+import App from './App';
+import './index.css';
+
+
+ReactDOM.render(<App/>,document.getElementById("root"))
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './image/logo192.png'
-import App from './App';
 import './index.css';
-
-
-ReactDOM.render(<App/>,document.getElementById("root"))
+ReactDOM.render(
+  <h1>React Basics</h1>,
+  document.getElementById('root')
+);
