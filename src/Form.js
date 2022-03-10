91352f7ae6752473e89cb10541808872e7946f2d
rranjan6 <rajeevbankey@gmail.com>
2022-03-11 00:32:47 +0530

    Form_checkbox

--- src/Form.js
+++ src/Form.js
@@ -4,15 +4,19 @@ export default function Form() {
 
     const [formData, setFormData] = React.useState({
         firstName: "",
-        lastName: ""
+        lastName: "",
+        email: "",
+        comments: "",
+        isFriendly: true
     })
     console.log(formData)
 
     function handleChange(event) {
+        const {name,value,type,checked} = event.target
         setFormData(preData=>{
             return {
                 ...preData,
-                [event.target.name] : event.target.value
+                [name] : type === "checkbox"? checked: value
             }
         })
     }
@@ -23,12 +27,33 @@ export default function Form() {
                 placeholder="FirstName"
                 onChange={handleChange}
                 name="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text" 
                 placeholder="LastName"
                 onChange={handleChange}
                 name="lastName"
+                value={formData.lastName}
+            />
+            <input
+                type="email" 
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+            />
+            <textarea
+                placeholder="Comments"
+                onChange={handleChange}
+                name="comments"
+                value={formData.comments}
+            />
+            <input
+                type="checkbox"
+                onChange={handleChange}
+                name="isFriendly"
+                checked={formData.isFriendly}
             />
         </form>
     )
