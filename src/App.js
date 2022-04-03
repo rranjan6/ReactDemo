c85f461a9695f571dde767837ec542adc2e2a484
rranjan6 <rajeevbankey@gmail.com>
2022-04-04 03:16:54 +0530

    Context without Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import HookCounter from "./Components/HookCounter";
-import DataFetching from "./Components/DataFetching";
+// import HookCounter from "./Components/HookCounter";
+// import DataFetching from "./Components/DataFetching";
+import ComponentA from "./ContextComponent/ComponentA";
+
+export const UserContext = React.createContext()
+export const ChannnelContext = React.createContext()
 
 export default function App() {
-    return(
+    return (
         <div>
             {/* <HookCounter/> */}
-            <DataFetching/>
+            {/* <DataFetching/> */}
+            <UserContext.Provider value="Rajeev">
+                <ChannnelContext.Provider value="Codevolution">
+                    <ComponentA />
+                </ChannnelContext.Provider>
+            </UserContext.Provider>
         </div>
     )
 }
